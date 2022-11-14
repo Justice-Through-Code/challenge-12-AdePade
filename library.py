c57d4a29cf19c578b3bc243ceff25f7fd77ba04e
Ade Cobbs <adecobbs@Ades-MacBook-Pro.local>
2022-11-14 00:20:11 -0500

    fix

--- library.py
+++ library.py
@@ -1,23 +1,23 @@
 from book import Book
-
-
 class Library():
     def __init__(self):
         """Initialize the empty book list"""
-        pass
-
-    def add_title(self):
+        self.books = []
+    def add_title(self, title , author):
         """Add a Book object with the given title and author to the book list"""
-        pass
-
+        my_book = Book(title, author)
+        self.books.append(my_book)
     def count_books(self):
         """Return the number of books currently in the booklist"""
-        pass
-
-    def remove_title(self):
+        return len(self.books)
+    def remove_title(self, title):
         """Remove a book from the book list"""
-        pass
-
+        for book in self.books:
+            if book.title == title:
+                self.books.remove(book)
     def is_empty(self):
         """Return True if the book list is empty, False if not"""
         return self.books == []
+    def display_books(self):
+        for book in self.books:
+            print(book)
